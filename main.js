0f51c530d9722de525c9a9ffbe14d6b6a10d7e5c
Yujin Yano <northwindapps@gmail.com>
2022-12-31 00:29:14 +0900

    multi lanes

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 window.addEventListener('DOMContentLoaded', function() {
     console.log('DOM is loaded')
 
-    var el = document.querySelector('#addprocess');
-    el.addEventListener('click', addProcess, false);
+    var addProcessBtn = document.querySelector('#addprocess');
+    var addBranchBtn = document.querySelector('#addbranch');
+    var mainUL = document.querySelector('#main');
+    
+    addProcessBtn.addEventListener('click', addProcess, false);
+    addBranchBtn.addEventListener('click', addBranch, false);
     function addProcess() {
 
+        console.log('process');
+    }
+
+    function addBranch() {
+        var li = document.createElement("li");
+        li.setAttribute("class", "branch"); 
+        li.innerHTML = '<ul></ul>';
+        mainUL.appendChild(li);
         console.log('process');
     }
 });
